70dde2d4bf857791bb51a9dee20f45280059753e
Woojoo Kim <129933678+ghvhdh333@users.noreply.github.com>
2024-07-01 22:10:21 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.60 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let result = []
+    for(let i = 0; i < n; i++){
+        let arr = []
+        for(let j = 0; j < n; j++){
+            if(i === j){
+                arr.push(1)
+            } else arr.push(0)
+        }
+        result.push(arr)
+    }
+    return result
+}
